c70e7f7e43ea6b86f8ee3210054e43dd50556ed8
mabatho <mabathomorebodi@gmail.com>
2019-09-11 10:43:12 +0200

    Initial commit

---
+++ FashionHubAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FashionHubAdmin
+//
+//  Created by MABATHO on 9/11/19.
+//  Copyright © 2019 Mabatho Morebodi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
